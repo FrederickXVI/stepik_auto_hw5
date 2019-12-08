f1d5d22d9b70b65d95e73bb7bfd9518a3b39015d
unknown <frederickxvi@gmail.com>
2019-12-08 14:34:22 +0300

    add ProductPage locators

--- pages/locators.py
+++ pages/locators.py
@@ -15,3 +15,11 @@ class LoginPageLocators():
     REGISTER_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
     REGISTER_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
     REGISTER_BUTTON = (By.CSS_SELECTOR, "#register_form .btn")
+
+
+class ProductPageLocators():
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
+    ALERT_PRODUCT_NAME = (By.CSS_SELECTOR, ".alert-success strong")
+    ALERT_PRODUCT_PRICE = (By.CSS_SELECTOR, ".alert-info strong")
