81df43636faad09490b9290db2b303065886f5ec
unknown <frederickxvi@gmail.com>
2019-12-08 22:03:33 +0300

    add func test_guest_cant_see_product_in_basket_opened_from_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,10 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
+import pytest as pytest
 
 
+@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)         # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
@@ -10,8 +13,24 @@ def test_guest_can_go_to_login_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
+
+@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    # Гость открывает главную страницу
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    # Переходит в корзину по кнопке в шапке сайта
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    # Ожидаем, что в корзине нет товаров
+    basket_page.should_not_be_basket_formset()
+    # Ожидаем, что есть текст о том что корзина пуста
+    basket_page.should_be_empty_basket_message()
