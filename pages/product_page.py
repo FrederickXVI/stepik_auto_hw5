8e328390292afa54b81ff82bf13c7a281b3438c0
unknown <frederickxvi@gmail.com>
2019-12-08 20:30:01 +0300

    add new functions

--- pages/product_page.py
+++ pages/product_page.py
@@ -14,10 +14,14 @@ class ProductPage(BasePage):
         self.solve_quiz_and_get_code()
         # проверить, что название продукта равно жирному начертанию текста в сообщении успеха и
         # стоимость продукта равна жирному начертанию текста в сообщении инфо
-        assert current_product_name == self.browser.find_element(*ProductPageLocators.ALERT_PRODUCT_NAME).text \
-            and current_product_price == self.browser.find_element(*ProductPageLocators.ALERT_PRODUCT_PRICE).text, \
+        assert current_product_name == self.browser.find_element(*ProductPageLocators.MESSAGE_PRODUCT_NAME).text \
+            and current_product_price == self.browser.find_element(*ProductPageLocators.MESSAGE_PRODUCT_PRICE).text, \
             "Название или стоимость продукта не соотвествует добавленному"
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
 
-
-
+    def success_message_is_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should be desappeared"
