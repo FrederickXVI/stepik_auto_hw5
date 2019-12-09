c5fc574e9c6b549955e7cbde19a7d8bcef52f997
unknown <frederickxvi@gmail.com>
2019-12-09 13:45:25 +0300

    save screenshot before browser close

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from datetime import datetime
 
 
 # добавляем параметр --language --browser_name для pytest
@@ -30,13 +31,19 @@ def browser(request):
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
+        browser.maximize_window()
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
         browser = webdriver.Firefox(firefox_profile=fp)
+        browser.maximize_window()
     else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+        print("Browser {} still is not implemented".format(browser_name))
     yield browser
+    # получаем переменную с текущей датой и временем в формате ГГГГ-ММ-ДД_ЧЧ-ММ-СС
+    now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    # делаем скриншот с помощью команды Selenium'а и сохраняем его с именем "screenshot-ГГГГ-ММ-ДД_ЧЧ-ММ-СС"
+    browser.save_screenshot('screenshot-%s.png' % now)
     print("\nquit browser..")
     browser.quit()
