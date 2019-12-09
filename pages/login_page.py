5fb736c8c425f1de1dab6b85d5c8a0b05f0378a8
unknown <frederickxvi@gmail.com>
2019-12-09 12:29:23 +0300

    add func register_new_user

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,16 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+    def register_new_user(self, email, password):
+        # находим поле ввода email для регистрации, передаем значение email
+        self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL).send_keys(email)
+        # находим поле ввода password для регистрации, передаем значение password
+        self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD).send_keys(password)
+        # находим поле ввода confirm password для регистрации, передаем значение password
+        self.browser.find_element(*LoginPageLocators.REGISTER_CONFIRM_PASSWORD).send_keys(password)
+        # нажимаем кнопку "регистрация"
+        self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
@@ -11,7 +21,6 @@ class LoginPage(BasePage):
     def should_be_login_url(self):
         # проверка на корректный url адрес
         assert "login" in self.browser.current_url, "Current url is not login url"
-
 
     def should_be_login_form(self):
         # проверка, что есть форма логина
