b243016f74fda0f24922bf3a1943ed7da1b6f605
unknown <frederickxvi@gmail.com>
2019-12-09 19:43:08 +0300

    add new login page locators

--- pages/locators.py
+++ pages/locators.py
@@ -18,11 +18,13 @@ class LoginPageLocators():
     LOGIN_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
     LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
     LOGIN_BUTTON = (By.CSS_SELECTOR, "#login_form .btn")
+    LOGIN_DANGER_ALERT = (By.CSS_SELECTOR, "#login_form .alert")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
     REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
     REGISTER_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
     REGISTER_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
     REGISTER_BUTTON = (By.CSS_SELECTOR, "#register_form .btn")
+    REGISTER_DANGER_ALERT = (By.CSS_SELECTOR, "#register_form .alert")
 
 
 class MainPageLocators():
